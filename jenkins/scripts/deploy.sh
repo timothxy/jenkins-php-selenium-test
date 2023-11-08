d32e12af34160edc15a87bc7a24a2b382d3239fa
Timothy <92201555+timothxy@users.noreply.github.com>
2023-11-08 14:18:42 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,6 +3,7 @@
 set +x
 docker run -d -p 80:80 --name my-apache-php-app -v /Users/chewtimothy/Downloads/GitHub/jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
+set -x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
