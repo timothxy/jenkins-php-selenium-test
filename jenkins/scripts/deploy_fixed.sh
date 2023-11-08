83d95f8d52ab6f0c0f43cd2277bdd9bd417e0cdf
Timothy <92201555+timothxy@users.noreply.github.com>
2023-11-08 14:58:33 +0800

    Update deploy_fixed.sh

--- jenkins/scripts/deploy_fixed.sh
+++ jenkins/scripts/deploy_fixed.sh
@@ -1,10 +1,10 @@
-#!usbinenv sh
+#!/usr/bin/env sh
 
 set +x
-docke un -d -p 80:80 --name my-apache-php-app -v UseschewtimothyDownloadsGitHubjenkins-php-selenium-testsc:vawwwhtml php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /Users/chewtimothy/Downloads/GitHub/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set -x
 
 echo 'Now...'
-echo 'Visit http:localhost to see you PHP application in action.'
+echo 'Visit http://localhost to see your PHP application in action.'
 
