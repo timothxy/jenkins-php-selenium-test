963a828b682e5437140c0cc5d97072267d2c9cd2
Timothy <92201555+timothxy@users.noreply.github.com>
2023-11-08 16:26:11 +0800

    Update deploy_fixed.sh

--- jenkins/scripts/deploy_fixed.sh
+++ jenkins/scripts/deploy_fixed.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 set +x
-docker run -d -p 80:80 --name my-apache-php-app -v "https://github.com/timothxy/jenkins-php-selenium-test/tree/master/src" php:7.2-apache
+docker run -d -p 5000:80 --name my-apache-php-app -v /Users/chewtimothy/Downloads/GitHub/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set -x
 
 echo 'Now...'
-echo 'Visit http://192.168.68.106 to see your PHP application in action.'
+echo 'Visit http://localhost:5000 to see your PHP application in action.'
 
