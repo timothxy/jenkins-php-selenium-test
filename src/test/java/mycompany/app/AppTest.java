b513a116ba2ab2ca18fcab04677eb50a370e87cb
Timothy <92201555+timothxy@users.noreply.github.com>
2023-11-09 16:15:49 +0800

    Update AppTest.java

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://ipaddr/src";
+	String url = "http://172.20.10.11";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
