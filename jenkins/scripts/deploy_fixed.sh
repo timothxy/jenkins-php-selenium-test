9f2a0c20b8ff9090ae3c06120cfec0602c42176f
Timothy <92201555+timothxy@users.noreply.github.com>
2023-11-09 17:17:24 +0800

    Update deploy_fixed.sh

--- jenkins/scripts/deploy_fixed.sh
+++ jenkins/scripts/deploy_fixed.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set +x
-docker run -d -p 80:80 --network jenkins --name my-apache-php-app -v jenkins-data:/data -v /var/www/html:/var/www/html php:7.2-apache && docker exec -it my-apache-php-app ln -s /data/workspace/jenkins-php-selenium-test/src /var/www/html
+docker run -d -p 80:80 --name my-apache-php-app -v jenkins-data:/data -v /var/www/html:/var/www/html php:7.2-apache && docker exec -it my-apache-php-app ln -s /data/workspace/jenkins-php-selenium-test/src /var/www/html
 sleep 1
 set -x
 
